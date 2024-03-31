d61cee9ad2dda49d27e7d4b92550c666468f4c71
Jiwon <hajw.study@gmail.com>
2024-03-31 18:17:28 +0900

    다이어리 sort 기능

--- src/app/components/Diary/Diary.tsx
+++ src/app/components/Diary/Diary.tsx
@@ -16,7 +16,7 @@ const data = [
     id: 2,
     title: "두번째",
     content: "내용내용",
-    date: "2024.03.22",
+    date: "2024.03.21",
     icon: "/images/battery/half_green.svg",
   },
   {
@@ -30,7 +30,7 @@ const data = [
     id: 4,
     title: "제목제목",
     content: "내용내용",
-    date: "2024.03.22",
+    date: "2024.03.27",
     icon: "/images/battery/one_green.svg",
   },
   {
@@ -44,14 +44,14 @@ const data = [
     id: 6,
     title: "제목제목",
     content: "내용내용",
-    date: "2024.03.22",
+    date: "2024.04.21",
     icon: "/images/battery/one_green.svg",
   },
   {
     id: 7,
     title: "제목제목",
     content: "내용내용",
-    date: "2024.03.22",
+    date: "2024.03.10",
     icon: "/images/battery/one_green.svg",
   },
 ];
@@ -64,15 +64,60 @@ export type Diary = {
   icon: string;
 };
 
-const Diary = () => {
+type Props = {
+  diaryMenuVisible: boolean;
+};
+
+const Diary = ({ diaryMenuVisible }: Props) => {
   const [diary, setDiary] = useState<Diary[]>(data);
+  const [filter, setFilter] = useState("");
 
   const handleClickDelete = (id: number) => {
     setDiary(diary.filter((item) => item.id !== id));
   };
 
+  const handleClickSort = (selectedFilter: string) => {
+    if (selectedFilter === "new" && filter !== "new") {
+      const newList = diary.slice().sort(function (a, b) {
+        return new Date(b.date).getTime() - new Date(a.date).getTime();
+      });
+
+      setDiary(newList);
+      setFilter("new");
+    } else if (selectedFilter === "old" && filter !== "old") {
+      const oldList = diary.slice().sort(function (a, b) {
+        return new Date(a.date).getTime() - new Date(b.date).getTime();
+      });
+
+      setDiary(oldList);
+      setFilter("old");
+    }
+  };
   return (
     <>
+      {diaryMenuVisible && (
+        <Menu>
+          <MenuWrap>
+            <div style={{ color: color.black, fontWeight: "bold" }}>
+              {diary.length} items
+            </div>
+            <FilterWrap>
+              <div
+                onClick={() => handleClickSort("new")}
+                className={filter === "new" ? "choice" : "common"}
+              >
+                최신순
+              </div>
+              <div
+                onClick={() => handleClickSort("old")}
+                className={filter === "old" ? "choice" : "common"}
+              >
+                오래된순
+              </div>
+            </FilterWrap>
+          </MenuWrap>
+        </Menu>
+      )}
       <Content>
         <DiaryItem diary={diary} handleClickDelete={handleClickDelete} />
       </Content>
@@ -81,6 +126,43 @@ const Diary = () => {
 };
 
 export default Diary;
+
+const Menu = styled.div`
+  width: 512px;
+  height: 50px;
+  margin-top: 4px;
+  background-color: ${color.white};
+  box-shadow: 10px 10px 20px rgba(0, 0, 0, 0.15);
+  box-sizing: border-box;
+
+  display: flex;
+  align-items: center;
+
+  font-size: 12px;
+`;
+
+const MenuWrap = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+  padding: 0 20px;
+
+  .choice {
+    color: ${color.blue};
+    cursor: pointer;
+  }
+  .common {
+    cursor: pointer;
+  }
+`;
+
+const FilterWrap = styled.div`
+  display: flex;
+  gap: 16px;
+
+  color: ${color.deepGray};
+  font-weight: bold;
+`;
 
 const Content = styled.div`
   width: 512px;
